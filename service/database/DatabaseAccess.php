08c9f34b0c28b642392a7924e9b21ba74280b16c
DorianRONDI <dorianrondi@gmail.com>
2023-11-16 12:11:10 +0100

    création DatabaseAcces.php

---
+++ service/database/DatabaseAccess.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\models;
+
+use PDO;
+use PDOException;
+
+class DataBaseAccess extends PDO
+{
+    private static $instance;
+    
+    private string $DBPASS;
+
+    private const DBHOST = 'localhost';
+    private const DBNAME = 'DonkeyStay';
+    private const DBUSER = 'root';
+
+    private function __construct()
+    {
+        $this->DBPASS = file_get_contents(dirname(__DIR__).'/core/Alpha/ignored/DBpass.secret');
+
+        $DataSourceName = 'mysql:dbname=' . self::DBNAME . ';host=' . self::DBHOST;
+
+        try{
+            parent::__construct($DataSourceName, self::DBUSER, $this->DBPASS);
+
+            $this->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8');
+            $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            //$this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
+
+    public static function getInstance ()
+    {
+        if(self::$instance === null){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
