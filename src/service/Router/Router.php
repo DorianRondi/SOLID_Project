be8774c742b52b4da3a3edbfbdbc4c0bef3f211a
Yoel <chicheporticheyoel@gmail.com>
2023-11-20 15:04:09 +0100

    corrections

--- src/service/Router/Router.php
+++ src/service/Router/Router.php
@@ -7,6 +7,9 @@ class Router
     {
         //// URL = ROOT/controller/method/parametres[] ////
         $uri = $_SERVER['REQUEST_URI'];
+        $numberOfCharsToRemove = 13 ; //(/solidproject)
+        $uri = substr($uri, $numberOfCharsToRemove);
+
         $lastCharacterOfURI = $uri[-1];
         $offset = 0;
         $length = -1;
@@ -16,28 +19,28 @@ class Router
         $params = explode('/', $params);
 
         array_shift($params);
-
-        if(strlen($params[0]) === 0){
+        if(empty($params)){
             $controller = new DefaultController;
             $controller->index();
-        }else{
-            $appController = '\\App\\Controller\\'.ucfirst(array_shift($params)).'Controller';
-            var_dump($appController);
-            $controller = new $appController;
-
-            $action = isset($params[0]) ? array_shift($params) : 'index';
-
-            if(!method_exists($controller, $action)){
-                http_response_code(404);
-                echo "Erreur 404 - La page n'existe pas";
-            }else{
-
-                //// call_user_func_array ////
-                /* je ne comprend pas vraiment comment ça marche */
-
-                isset($params[0]) ? call_user_func_array([$controller,$action],$params) : $controller->$action();
-                //isset($params[0]) ? $controller->$action($params) : $controller->$action();// vielle version
-            }
+            return;
         }
+
+        $appController = '\\App\\Controller\\'.ucfirst(array_shift($params)).'Controller';
+        $controller = new $appController;
+
+        $action = isset($params[0]) ? array_shift($params) : 'index';
+
+        if(!method_exists($controller, $action)){
+            http_response_code(404);
+            echo "Erreur 404 - La page n'existe pas";
+        }else{
+
+            //// call_user_func_array ////
+            /* je ne comprend pas vraiment comment ça marche */
+
+            isset($params[0]) ? call_user_func_array([$controller,$action],$params) : $controller->$action();
+            //isset($params[0]) ? $controller->$action($params) : $controller->$action();// vielle version
+        }
+
     }
 }
